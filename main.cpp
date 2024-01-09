502480f6a542fa65c8a7991d44370e1608bdd578
jksully <jksully@umich.edu>
2024-01-09 15:41:16 -0500

    Added relevant json example for P1

--- main.cpp
+++ main.cpp
@@ -3,18 +3,25 @@
 #include <cassert>
 #include <vector>
 #include <fstream>
-#include <algorithm>
+#include <format>
+#include <filesystem>
 #include "mros_json.hpp"
 
-int main() {
-    std::ifstream ifs("pose.json");
-    if (!ifs.is_open()) {
-        std::cerr << "Error opening file" << std::endl;
-        return 1;
-    }
-    std::string jsonString(std::istreambuf_iterator<char>{ifs}, {});
-    Json json = Json::fromString(jsonString);
 
-    std::cout << json.toString() << std::endl;
+int main() {
+    std::string directory = "messages";
+    std::filesystem::path absolute_path = std::filesystem::current_path() / directory;
+    for (auto const& entry : std::filesystem::directory_iterator(absolute_path)) {
+        std::ifstream ifs(entry.path());
+        if (!ifs.is_open()) {
+            std::cerr << "Error opening file" << std::endl;
+            return 1;
+        }
+        std::string jsonString(std::istreambuf_iterator<char>{ifs}, {});
+        Json json = Json::fromString(jsonString);
+
+        std::cout << json.toString() << std::endl;
+    }
+
     return 0;
 }
